6bde813d8b6d9d694b7cda4f5cb7488b6376a07f
Devon Hockley <devon@hockley.ca>
2018-01-29 10:02:53 -0700

    Tried to fix buttons not appearing

--- app/src/main/java/com/example/describe/term_project/BlankScreen.java
+++ app/src/main/java/com/example/describe/term_project/BlankScreen.java
@@ -39,13 +39,22 @@ public class BlankScreen extends AppCompatActivity {
         @SuppressLint("InlinedApi")
         @Override
         public void run() {
-            // Delayed removal of status and navigation bar
 
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
+                // Delayed removal of status and navigation bar
 
-        }
+                // Note that some of these constants are new as of API 16 (Jelly Bean)
+                // and API 19 (KitKat). It is safe to use them, as they are inlined
+                // at compile-time and do nothing on earlier devices.
+                mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+            }
+
+
+
     };
     private View mControlsView;
     private final Runnable mShowPart2Runnable = new Runnable() {
