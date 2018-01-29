f67ed18c09eebc89b10ac66fa4992a6f9e86d80d
Devon Hockley <devon@hockley.ca>
2018-01-29 10:52:35 -0700

    idk what im doing anymore

--- app/src/main/java/com/example/describe/term_project/BlankScreen.java
+++ app/src/main/java/com/example/describe/term_project/BlankScreen.java
@@ -98,7 +98,7 @@ public class BlankScreen extends AppCompatActivity {
 
         mVisible = true;
         mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content_controls);
+        mContentView = findViewById(R.id.screen);
 
 
         // Set up the user interaction to manually show or hide the system UI.
