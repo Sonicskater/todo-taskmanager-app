1199be1e92d288bb667d5355eeaa69131e61f52d
Devon Hockley <devon@hockley.ca>
2018-01-30 14:22:10 -0700

    changed project package to correct for human error resulting from google play requirements

---
+++ app/src/main/java/com/describe/taskmanager/emptyScreen.java
@@ -0,0 +1,13 @@
+package com.describe.taskmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class emptyScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty_screen);
+    }
+}
