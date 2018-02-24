d0a5b655c8dc7afed6370f746122a1ff8667a876
bencook <benjamin.cook@ucalgary.ca>
2018-02-24 10:48:27 -0700

    added save and delete event 'onclick'  methods

--- app/src/main/java/com/describe/taskmanager/TaskEventView.java
+++ app/src/main/java/com/describe/taskmanager/TaskEventView.java
@@ -2,6 +2,8 @@ package com.describe.taskmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class TaskEventView extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class TaskEventView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_event_view);
+    }
+
+    public void onDelete(View view) {
+        Log.d("onDelete", "onDelete: ");
+    }
+
+    public void onSave(View view) {
+        Log.d("onSave", "onSave: ");
     }
 }
