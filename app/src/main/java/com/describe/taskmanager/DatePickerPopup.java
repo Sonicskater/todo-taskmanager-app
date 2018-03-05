26aed04db2658c3ca3120d4017a9d1415b8e7d99
Devon Hockley <devon@hockley.ca>
2018-03-05 12:13:32 -0700

    Deprecated Time and Date Popup classes.

--- app/src/main/java/com/describe/taskmanager/DatePickerPopup.java
+++ app/src/main/java/com/describe/taskmanager/DatePickerPopup.java
@@ -3,6 +3,7 @@ package com.describe.taskmanager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+@Deprecated
 public class DatePickerPopup extends AppCompatActivity {
 
     //overridden method, initializes the date picker popup activity
