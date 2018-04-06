895a46798bf4af26a4e8f9933f6430979642ada0
Devon Hockley <devon@hockley.ca>
2018-04-06 12:32:48 -0600

    added image support to task. Cant be changed yet

--- app/src/main/java/com/describe/taskmanager/FBStorageAgent.java
+++ app/src/main/java/com/describe/taskmanager/FBStorageAgent.java
@@ -1,11 +1,18 @@
 package com.describe.taskmanager;
 
+import android.graphics.Bitmap;
+
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
+import java.util.Random;
+
 //Currently un-used, meant for future feature
 @SuppressWarnings("FieldCanBeLocal")
 public class FBStorageAgent {
@@ -36,11 +43,36 @@ public class FBStorageAgent {
 
 
 
+
         }
         return fileList;
     }
 
-    public void downloadFile(String filePath){
+    public void downloadFile(String filePath, final ImageInterface callingObject){
+
+        StorageReference islandRef = storageRef.child(FirebaseAuth.getInstance().getCurrentUser().getUid()+filePath);
+
+        final long ONE_MEGABYTE = 1024 * 1024;
+        islandRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                callingObject.receiveBitmap(ImageUtils.convertByteArray(bytes));
+            }
+        });
+    }
+    public void uploadImage(final TaskEvent event,Bitmap image){
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        String path = "/"+Integer.toString((new Random()).nextInt())+".jpg/";
+        event.setPath(path);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.JPEG, 40, stream);
+        UploadTask upload = storageRef.child(uid+path).putBytes(stream.toByteArray());
+        upload.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                event.setDlURL(taskSnapshot.getDownloadUrl());
+            }
+        });
 
     }
 }
